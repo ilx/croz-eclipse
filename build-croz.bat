ffd16339498bbc7af08baf304966d2162590f331
Ivica Loncar <iloncar.ml+github@gmail.com>
2012-03-16 13:28:18 +0100

    Added roaming profile support.

--- build-croz.bat
+++ build-croz.bat
@@ -1,4 +1,5 @@
 set MAVEN_OPTS="-Declipse.p2.mirrors=false"
 set INSTALL_DIR="C:/tmp/eclipse-3.7.2"
-mvn -s settings.croz.xml -Pcroz -Dajdt -Danyedit -Declemma -Dmoreunit -Dsts -Dsubclipse -Dbuild.destination=%INSTALL_DIR% compile
+
+mvn -s settings.croz.xml -Pcroz -Dajdt -Danyedit -Declemma -Dmoreunit -Dsts -Dsubclipse -Dbuild.destination=%INSTALL_DIR% -Dbuild.p2.roaming='-roaming' -Dbuild.p2.bundlepool=%INSTALL_DIR% compile
 
--- build-croz.sh
+++ build-croz.sh
@@ -5,29 +5,37 @@ export MAVEN_OPTS="-Declipse.p2.mirrors=false"
 
 # debug maven:
 #export MAVEN_OPTS="-Declipse.p2.mirrors=false -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000"
-# Maven properties can be overwritten. Otherwise default ones (from settings file) will be used.
-# Here's an example how to override default value of eclipse.mirror.url property:
-
 # install just eclipse with m2e:
 #mvn -s settings.croz.xml -Pcroz -Dbuild.destination=$INSTALL_DIR -Declipse.mirror.url="http://deploy.lan.croz.net/eclipseMirror" compile
 
 #define default values
 EFLAG=JEE
 VERBOSITY=0
+SETTINGS=settings.croz.xml
+PROFILE=-Pcroz
 
 function usage(){
-USAGE=$(
 cat <<EOF
-	-h 		this help
-	-d 		install destination directory
-	-e [JDT|JEE|OTDT] 	installation environment (JEE,OT)
-	-v		verbosity
+	-h 			this help
+	-d 			install destination directory
+	-e [JDT|JEE|OTDT]  	installation environment (JEE,OT)
+	-l                 	use local settings 
+	-p 			use bundle pool
+	-r			use roaming profile (allows you to copy installation to some other dir)
+	-v			verbosity
+
+Examples:
+
+Install JEE version of eclipse using CROZ repositories, not using roaming profile, bundle pool same as destination
+	> ./build-croz.sh -v -e JEE -d /opt/java/eclipse/e37/scalaide
+
+Install JDT version of eclipse using local repositories with roaming profile, bundle pool same as destination
+	> ./build-croz.sh -v -l -e JDT -d /opt/java/eclipse/e37/test -r 
 EOF
-)
-	echo $USAGE
+	exit
 }
 
-while getopts "d:hev" OPTION; do
+while getopts "d:e:hlprv" OPTION; do
 	case "$OPTION" in
 		d)
 			DFLAG="$OPTARG"
@@ -36,11 +44,22 @@ while getopts "d:hev" OPTION; do
 		e)
 			EFLAG="$OPTARG"
 			;;
+		h)
+			usage
+			;;
+		l)      
+			#OPTS="-o -X"
+			PROFILE=-Plocal
+			SETTINGS=settings.local.xml
+			;;
 		v)
 			VERBOSE=1
 			;;
-		h)
-			usage
+		p)
+			PFLAG=1
+			;;
+		r)
+			RFLAG=1
 			;;
 		*)
 			usage
@@ -48,9 +67,16 @@ while getopts "d:hev" OPTION; do
 	esac
 done
 
+echo "$EFLAG"
+
 function validate() {
+	echo "BEGIN: validate"
 	if [ -z "$DFLAG" ]; then
 		echo "Destination must be defined"
+		BREAK=1
+	fi
+	if [ -z "$EFLAG" ]; then
+		echo "Environment must be defined"
 		BREAK=1
 	fi
 
@@ -58,28 +84,35 @@ function validate() {
 		usage
 		exit
 	fi
+	echo "END: validate"
 }
 
 
 function install() {
+	echo "BEGIN: install"
 	case "$EFLAG" in
 	JEE)
-		PLUGINS="-Pcroz -Dajdt -Danyedit -Dcheckstyle -Declemma -Dmoreunit -Dsts -Dsubclipse"
+		PLUGINS="-Dajdt -Danyedit -Dcheckstyle -Declemma -Dmoreunit -Dsts -Dsubclipse"
 		;;
 	OTDT)
 		# currently OTDT doesn;t play well with AJDT (required by STS)
-		PLUGINS="-Pcroz -Danyedit -Dcheckstyle -Declemma -Dmoreunit -Dotdt -Dsubclipse"
+		PLUGINS="-Danyedit -Dcheckstyle -Declemma -Dmoreunit -Dotdt -Dsubclipse"
 		;;
 	JDT)
-		PLUGINS="-Pcroz"
+		PLUGINS=""
 		;;
 	*)
 		usage
 		exit
 		;;
 	esac
-	echo mvn -s settings.croz.xml $PLUGINS -Dbuild.destination=$INSTALL_DIR -Dbuild.p2.bundlepool=$INSTALL_DIR/../p2 compile
-	mvn -s settings.croz.xml $PLUGINS -Dbuild.destination=$INSTALL_DIR -Dbuild.p2.bundlepool=$INSTALL_DIR/../p2 compile
+	#BUNDLE_POOL="-Dbuild.p2.bundlepool=$INSTALL_DIR"
+	BUNDLE_POOL="-Dbuild.p2.bundlepool=$INSTALL_DIR"
+	ROAMING="-Dbuild.p2.roaming=''"
+	if [ -n "$PFLAG" ]; then BUNDLE_POOL="-Dbuild.p2.bundlepool=$INSTALL_DIR/../p2" ;fi
+	if [ -n "$RFLAG" ]; then ROAMING="-Dbuild.p2.roaming='-roaming'" ; fi
+	echo mvn $OPTS $PROFILE -s $SETTINGS $PLUGINS -Dbuild.destination=$INSTALL_DIR $ROAMING $BUNDLE_POOL compile
+	mvn $OPTS $PROFILE -s $SETTINGS $PLUGINS -Dbuild.destination=$INSTALL_DIR $ROAMING $BUNDLE_POOL compile
 }
 
 validate
